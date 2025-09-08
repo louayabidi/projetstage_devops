6cd97543b437aadfa3bdb9cf9d217c7a0c556e0e
louayabidi <louay.abidi@esprit.tn>
2025-09-08 14:31:34 +0100

    fixing some stuff4

--- front/jest.config.cjs
+++ front/jest.config.cjs
@@ -6,19 +6,17 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|svg)$': 'jest-transform-stub'
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!maath|react-github-btn/)'
+    '/node_modules/(?!maath/)'
   ],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleNameMapper: {
     '^components/(.*)$': '<rootDir>/src/dashboard/components/$1',
-    '^assets/(.*)$': [
-      '<rootDir>/src/dashboard/assets/$1',
-      '<rootDir>/src/assets/$1'
-    ],
+    '^assets/(.*)$': '<rootDir>/src/assets/$1',
     '^hoc/(.*)$': '<rootDir>/src/hoc/$1',
     '^utils/(.*)$': '<rootDir>/src/utils/$1',
     '^examples/(.*)$': '<rootDir>/src/dashboard/examples/$1',
-    // 👇 Add this
+
+    // 👇 Add this line for CSS/SCSS handling
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
   },
   moduleDirectories: ['node_modules', 'src']
